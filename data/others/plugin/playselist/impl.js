fedac489bb0defb4a08e702db16ec53940862102
catoriinu <catoriinu8190@gmail.com>
2025-05-17 18:47:07 +0900

    [playselist]プラグイン：parseInt に基数指定を追加、Howl 未生成時のガードを導入、waitForHowl タイムアウトで即時 reject() するよう変更

--- data/others/plugin/playselist/impl.js
+++ data/others/plugin/playselist/impl.js
@@ -37,7 +37,7 @@ function PlaySeListSubject() {
  */
 function PlaySeObserver(mp) {
   // 引数のintervalを数値に変換。指定がなければデフォルトで0を入れる
-  mp.interval = ('interval' in mp) ? parseInt(mp.interval) : 0;
+  mp.interval = ('interval' in mp) ? parseInt(mp.interval, 10) : 0;
   // [playse]に渡すための引数をプロパティに保持しておく
   this.mp = mp;
 
@@ -54,39 +54,59 @@ function PlaySeObserver(mp) {
     return new Promise((resolve, reject) => {
       // このPlaySeObserverに保持されていたSEを再生する
       TYRANO.kag.ftag.startTag("playse", this.mp);
-      // 再生中のHowlオブジェクトを格納する
-      let playingSoundObj = TYRANO.kag.tmp.map_se[this.mp.buf];
-      // 再生完了後に待機すべきインターバルの時間を、プロパティから取得する
-      let intervalAfterPlaySe = this.mp.interval;
 
-      // 再生終了チェック
-      const intervalId = setInterval(() => {
-        if (playingSoundObj !== TYRANO.kag.tmp.map_se[this.mp.buf]) {
-          // 再生中のHowlオブジェクトが、格納しておいたHowlオブジェクトと異なっていた場合、rejectして処理を中断する
-          // クリック等の操作でシナリオが先に進んだことにより別のSEが再生中なので、[playselist]を継続してはいけないため
-          clearInterval(intervalId);
-          reject();
-        } else if (!TYRANO.kag.tmp.map_se[this.mp.buf].playing()) {
+      // Howlオブジェクトをロードするためのポーリング処理を行う
+      // ただしmaxChecks * checkInterval(ms)の時間ぶんポーリングしてもロードが完了しない場合は[playselist]自体を中断する
+      // 環境によってロードに時間がかかる場合は、maxChecksやcheckIntervalの値を調整すること
+      const maxChecks = 20;     // 最大チェック回数
+      const checkInterval = 50; // チェック間隔(ms)
+      let checks = 0;
+
+      const buf = this.mp.buf;
+      const waitForHowl = () => {
+        const howl = TYRANO.kag.tmp.map_se[buf];
+        // state() があればロード済み、なければ未ロード or 未生成
+        if (howl && typeof howl.state === 'function' && howl.state() === 'loaded') {
+          startMonitoring(howl);
+        } else if (checks++ < maxChecks) {
+          setTimeout(waitForHowl, checkInterval);
+        } else {
+          return reject(new Error(`[playselist] Howlオブジェクトのロードがタイムアウトしたため中断 buf=${buf}`));
+        }
+      };
+
+      // 監視ループ開始
+      const startMonitoring = (howl) => {
+        // 再生完了後に待機すべきインターバルの時間を、プロパティから取得する
+        let intervalAfterPlaySe = this.mp.interval;
+
+        // 再生終了チェック
+        const intervalId = setInterval(() => {
+          if (howl !== TYRANO.kag.tmp.map_se[buf]) {
+            // 再生中のHowlオブジェクトが、格納しておいたHowlオブジェクトと異なっていた場合、rejectして処理を中断する
+            // クリック等の操作でシナリオが先に進んだことにより別のSEが再生中なので、[playselist]を継続してはいけないため
+            clearInterval(intervalId);
+            reject(new Error(`[playselist] 別のSEの再生が開始されているため中断 buf=${buf}`));
+            return;
+          }
+
           // Howlオブジェクトの再生が完了している場合
           // 注意：
           // sprite_time=""を指定しておりかつ終了時間が実際のファイルの再生時間よりも長い場合、playing()がfalseになるのはsprite_timeの終了時間の方
-
-          // 再生完了後に待機すべきインターバルの時間ぶんの待機も完了したら、処理を正常終了する
-          if (intervalAfterPlaySe <= 0) {
-            clearInterval(intervalId);
-            resolve();
+          if (!howl.playing()) {
+            // 再生完了後に待機すべきインターバルの時間ぶんの待機も完了したら、処理を正常終了する
+            if (intervalAfterPlaySe <= 0) {
+              clearInterval(intervalId);
+              resolve();
+            } else {
+              intervalAfterPlaySe -= this.loopTime;
+            }
           }
+        }, this.loopTime); // loopTimeのミリ秒分待機してから、再度再生終了チェックを行う
+      };
 
-          // intervalAfterPlaySeがloopTimeよりも短い場合、
-          // このままでは残り時間が先に終わるのに、再生終了チェックが行われるまで無駄に待機してしまうため、
-          // intervalAfterPlaySeでloopTimeを上書きすることで、当初のインターバルの時間ピッタリで処理終了できるようにする
-          if (intervalAfterPlaySe < this.loopTime) {
-            this.loopTime = intervalAfterPlaySe;
-          }
-          // intervalAfterPlaySeの残り時間からloopTimeぶんを減らす
-          intervalAfterPlaySe -= this.loopTime;
-        }
-      }, this.loopTime); // loopTimeのミリ秒分待機してから、再度再生終了チェックを行う
+      // 最初のチェックをキック
+      waitForHowl();
     });
-  }
+  };
 }
